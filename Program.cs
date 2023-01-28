260c93b307732a20e0df9789fd0a69e77382aed1
zayka <120934899+zayka-12@users.noreply.github.com>
2023-01-28 22:49:46 +0300

    comment

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.WriteLine($"Ряд чисел преобразует в мвссив");
+Console.Write("Введите ряд чисел, разделенных запятой :  ");
+string? seriesOfNumbers = Console.ReadLine();
+
+seriesOfNumbers = seriesOfNumbers + ",";
+
+string RemovingSpaces (String series)
+{
+   string seriesNew = "";
+   for (int i = 0; i < series.Length; i++)
+  {
+     if (series[i] != ' ')
+     {
+       seriesNew += seriesOfNumbers[i];
+     }
+  }
+return seriesNew;   
+}
